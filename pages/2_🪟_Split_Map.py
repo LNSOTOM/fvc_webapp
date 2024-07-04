ee11925792e8fcb87774b9ba6539156ef5a15747
lnsotom <lauransotomayor5@gmail.com>
2024-07-04 13:00:12 +1000

    layer

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -16,12 +16,28 @@ st.sidebar.image(logo)
 
 st.title("Split-panel Map")
 
-with st.expander("See source code"):
-    with st.echo():
-        m = leafmap.Map()
-        m.split_map(
-            left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
-        )
-        m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+# with st.expander("See source code"):
+#     with st.echo():
+#         m = leafmap.Map()
+#         m.split_map(
+#             left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
+#         )
+#         m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
 
-m.to_streamlit(height=700)
+# m.to_streamlit(height=700)
+import random
+
+m = leafmap.Map(center=[0, 0], zoom=2)
+
+url = "https://huggingface.co/datasets/lauransotomayor/eco_composition/blob/main/mask_fvc_3072.geojson"
+
+
+def random_color(feature):
+    return {
+        "color": "black",
+        "fillColor": random.choice(["red", "yellow", "green", "orange"]),
+    }
+
+
+m.add_geojson(url, layer_name="Countries", style_callback=random_color)
+m
